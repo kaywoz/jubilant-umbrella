7af4c02ead7b4cf7787335da3421b0416a816839
ka <kaywoz@secretemail>
2020-04-29 19:32:16 +0200

    debugging

--- W10GenericBox.ps1
+++ W10GenericBox.ps1
@@ -26,7 +26,6 @@ $DefenderExcludeDirs = 'c:\tmp'
  #Start transcript and log
  Start-Transcript -Path .\$ScriptName.txt -NoClobber
 
- 
  #Start install by splash etc
   Write-Host "`n"
   Write-Host " ____________________________________________________________________________ " -ForegroundColor White 
@@ -60,7 +59,7 @@ $DefenderExcludeDirs = 'c:\tmp'
 
 $NewHostname = Read-Host 'Hostname ?'
 
-Write-Host "***Changing hostname from "$CurrentHostname" to: " $NewHostname  -ForegroundColor Green
+Write-Host "***Changing hostname from"$CurrentHostname" to: "$NewHostname  -ForegroundColor Green
 Rename-Computer -NewName $NewHostname | Out-Null  
 
 $thisPCIconRegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel"
@@ -82,6 +81,73 @@ try {
 catch {
     Write-Warning -Message "ERROR: Directories already exist ----> Skipping.";
     }
+
+Write-Host "***Installing BoxStarter..."  -ForegroundColor Green
+. { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force | Out-Null 
+
+Write-Host "***Re-checking chocolatey..."  -ForegroundColor Green
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) | Out-Null 
+
+Write-Host "***Installing choco packages..."  -ForegroundColor Green
+
+cinst 7zip.install -y | Out-Null 
+Write-Host "*** 7zip installed..."  -ForegroundColor Green
+cinst vlc -y | Out-Null 
+Write-Host "*** vlc installed..."  -ForegroundColor Green
+cinst notepadplusplus.install -y | Out-Null 
+Write-Host "*** notepadplusplus installed..."  -ForegroundColor Green
+cinst powershell -y | Out-Null 
+Write-Host "*** powershell installed..."  -ForegroundColor Green
+cinst curl -y | Out-Null 
+Write-Host "*** curl installed..."  -ForegroundColor Green
+cinst wget -y | Out-Null 
+Write-Host "*** wget installed..."  -ForegroundColor Green
+cinst python3 -y | Out-Null 
+Write-Host "*** python3 installed..."  -ForegroundColor Green
+cinst python -y | Out-Null 
+Write-Host "*** python installed..."  -ForegroundColor Green
+cinst sumatrapdf.install -y | Out-Null 
+Write-Host "*** sumatrapdf installed..."  -ForegroundColor Green
+cinst chromium -y | Out-Null 
+Write-Host "*** chromium installed..."  -ForegroundColor Green
+cinst winscp.portable -y | Out-Null 
+Write-Host "*** winscp installed..."  -ForegroundColor Green
+cinst treesizefree -y | Out-Null 
+Write-Host "*** treesizefree installed..."  -ForegroundColor Green
+##cinst microsoft-windows-terminal -y | Out-Null // broken atm
+
+Write-Host "***Changing Explorer behaviour"  -ForegroundColor Green
+# will expand explorer to the actual folder you're in
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
+#adds things back in your left pane like recycle bin
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+#opens PC to This PC, not quick access
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+
+Write-Host "***Changing Taskbar behaviour"  -ForegroundColor Green
+# will expand explorer to the actual folder you're in
+Set-TaskbarOptions -Size Small -Lock -Dock Bottom -Combine Always
+
+#Cleanup
+Write-Host "***Removing stuff..."  -ForegroundColor Green
+Get-ChildItem C:\Users\Public\Desktop | Remove-Item
+Get-ChildItem C:\Users\$CurrentUser\Desktop | Remove-item
+Clear-RecycleBin -Force
+
+Write-Host "***Setting wallpaper...." -ForegroundColor Green
+(New-Object System.Net.WebClient).DownloadFile('https://git.io/JfYWM','c:\tools\scripts\Set-Wallpaper.ps1')
+Write-Host "***Scheduling wallpaper refresh task...." -ForegroundColor Green
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-nologo -noninteractive -WindowStyle Hidden c:\tools\scripts\Set-Wallpaper.ps1 Colour Black"
+$trigger =  New-ScheduledTaskTrigger -AtLogOn
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "WallpaperRefresh" -Description "Refreshes wallpaper" | Out-Null
+
+Write-Host "***Enabling W10 DevMode..."  -ForegroundColor Green
+#Enable developer mode on the system
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
+
+Write-Host "***Showing hidden files and such..."  -ForegroundColor Green
+# Show hidden files, Show protected OS files, Show file extensions
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
 
 Write-Host "***Excluding C:\tmp from WinDefender..."  -ForegroundColor Green
 Add-MpPreference -ExclusionPath $DefenderExcludeDirs
@@ -113,39 +179,9 @@ catch {
     New-ItemProperty -Path $path -Name 'VisualFXSetting' -Value 2 -PropertyType 'DWORD'
     }
 
-Write-Host "***Installing BoxStarter..."  -ForegroundColor Green
-. { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force | Out-Null 
-
-Write-Host "***Changing Explorer behaviour"  -ForegroundColor Green
-# will expand explorer to the actual folder you're in
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
-#adds things back in your left pane like recycle bin
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
-#opens PC to This PC, not quick access
-Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
-
-Write-Host "***Changing Taskbar behaviour"  -ForegroundColor Green
-# will expand explorer to the actual folder you're in
-Set-TaskbarOptions -Size Small -Lock -Dock Bottom -Combine Always
-
-Write-Host "***Setting wallpaper...." -ForegroundColor Green
-(New-Object System.Net.WebClient).DownloadFile('https://git.io/JfYWM','c:\tools\scripts\Set-Wallpaper.ps1')
-Write-Host "***Scheduling wallpaper refresh task...." -ForegroundColor Green
-$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-nologo -noninteractive -WindowStyle Hidden c:\tools\scripts\Set-Wallpaper.ps1 Colour Black"
-$trigger =  New-ScheduledTaskTrigger -AtLogOn
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "WallpaperRefresh" -Description "Refreshes wallpaper" | Out-Null
-
-Write-Host "***Enabling W10 DevMode..."  -ForegroundColor Green
-#Enable developer mode on the system
-Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
-
-Write-Host "***Showing hidden files and such..."  -ForegroundColor Green
-# Show hidden files, Show protected OS files, Show file extensions
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
-
 Write-Host "***Prepping for WSL..."  -ForegroundColor Green
 , 'n' * 1  | powershell "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux" | Out-Null 
-cinst -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
+cinst -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'" | Out-Null 
 RefreshEnv
 
 Write-Host "***Disabling hibernation...."  -ForegroundColor Green
@@ -160,41 +196,17 @@ Try {
     Write-Warning -Message "***Unable to set power plan to high performance"
 }
 
-Write-Host "***Re-checking chocolatey..."  -ForegroundColor Green
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) |out-null
-
-Write-Host "***Installing choco packages..."  -ForegroundColor Green
-cinst 7zip.install -y
-cinst vlc -y
-cinst notepadplusplus.install -y
-cinst powershell -y
-cinst curl -y 
-cinst wget -y 
-cinst python3 -y
-cinst python -y
-cinst sumatrapdf.install -y
-cinst chromium -y
-cinst winscp.portable -y
-cinst treesizefree -y
-cinst microsoft-windows-terminal -y
-
 Write-Host "***Resetting System Install base..."  -ForegroundColor Green
 DISM.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 
 Write-Host "***Updating Windows...."  -ForegroundColor Green
-Install-WindowsUpdate -SuppressReboots $true
+Install-WindowsUpdate
 
 Write-Host "***Updating PS help...."  -ForegroundColor Green
-Update-Help -ErrorAction SilentlyContinue
+Update-Help -ErrorAction SilentlyContinue | Out-Null 
 
 Write-Host "***Compacting non-essential and duplicate OS-files..."  -ForegroundColor Green
 Compact.exe /CompactOS:always
-
-#Cleanup
-Write-Host "***Removing stuff..."  -ForegroundColor Green
-Get-ChildItem C:\Users\Public\Desktop | Remove-Item
-Get-ChildItem C:\Users\$CurrentUser\Desktop | Remove-item
-Clear-RecycleBin -Force
 
 Write-Host "***Pulling and prepping for debloat...."  -ForegroundColor Green
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Sycnex/Windows10Debloater/master/Windows10SysPrepDebloater.ps1'))
